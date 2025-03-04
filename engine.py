ea7cfa0c998d3e12ebca166dd4db70b0450803d4
huolter <gualteriokk@gmail.com>
2025-03-05 00:04:23 +0100

    added domain detail

--- engine.py
+++ engine.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gualterio.com"],
+    allow_origins=["https://gualterio.com/infinal/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
